0a5585d2f11711d8b415ff5bd54d338966248a41
Abhinav Sonone <95363279+aps0611@users.noreply.github.com>
2022-11-30 18:36:57 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+
+header = st.container()
+userInput = st.container()
+output = st.container()
+
+with header:
+	st.title('Welcome to my awesome Streamlit Project!')
+	st.subheader('USECASE: Substraction of two given numbers')
+
+
+with userInput:
+	st.subheader('Please enter the input')
+	st.text('The output is a - b')
+	num1 = st.number_input(label="Enter a",step=1.,format="%.2f")
+	num2 = st.number_input(label="Enter b",step=1.,format="%.2f")
+
+
+def sub(num1,num2):
+	ans = num1 - num2
+	return ans
+
+ans = sub(num1,num2)
+
+with output:
+	st.subheader('Please see the output below  :')
+	st.markdown(f'Answer is {ans}')
